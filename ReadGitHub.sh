3b58e2a545b2f5e3b5d690c74d6fa1ac31360392
mirachan010 <mirachan010@gmail.com>
2020-08-27 00:51:10 +0900

    削除に10秒以上かかる場合は追加で10秒じゃなくってそのままで問題ない

--- ReadGitHub.sh
+++ ReadGitHub.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 my-repositories $1 > ./$1.list
+Max=`cat $1.list |wc -l`
+COUNT=0
 while read line
 do
-    sleep 10
     git clone git://github.com/$1/$line --depth=1
     if [ -d ./$line/autoload -o -d ./$line/plugin ];then
         echo $1/$line >> list
     fi
-    rm -rf ./$line
+    rm -rf ./$line &
+    sleep 10 &
+    wait
+    COUNT=$(( COUNT + 1 ))
+    echo $COUNT/$Max
 done < $1.list
 rm ./$1.list
 echo finish
