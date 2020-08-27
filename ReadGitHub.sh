5b902d45e90fcb4284797ac589a4112526a32f4c
mirachan010 <mirachan010@gmail.com>
2020-08-27 16:23:54 +0900

    vimというのなのリポジトリを持つものに消されるだなんて

--- ReadGitHub.sh
+++ ReadGitHub.sh
@@ -2,17 +2,19 @@
 my-repositories $1 > ./$1.list
 Max=`cat $1.list |wc -l`
 COUNT=0
+VCOUNT=0
 while read line
 do
     git clone git://github.com/$1/$line --depth=1
     if [ -d ./$line/autoload -o -d ./$line/plugin ];then
         echo $1/$line >> list
+        VCOUNT=$(( VCOUNT + 1 ))
     fi
+    COUNT=$(( COUNT + 1 ))
+    echo $COUNT/$Max -- $VCOUNT
     rm -rf ./$line &
     sleep 10 &
     wait
-    COUNT=$(( COUNT + 1 ))
-    echo $COUNT/$Max
 done < $1.list
 rm ./$1.list
 echo finish
