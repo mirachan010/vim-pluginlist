cf0aafb65a76cc42b0510aa5cd5af472cfb5e424
mirachan010 <mirachan010@gmail.com>
2020-09-20 17:04:04 +0900

    tabで複数選択可能

--- MakeInstallListForDein.sh
+++ MakeInstallListForDein.sh
@@ -2,7 +2,7 @@
 while :
 do
     cd ./ReadMe
-    package=$( fzf --preview "head -100 {}" | awk '{print $1}' )
+    package=$( fzf -m --preview "head -100 {}" | awk '{print $1}' )
     cd ../
     if [ -n "$package" ]; then
         echo "$package" | sed -e "s/$/\'/" -e "s/^/[[plugins]]\nrepo='/" >> ./list.toml
