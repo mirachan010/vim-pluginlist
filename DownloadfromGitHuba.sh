d8d8ef0d5edc5f55d59e1ce32a1fb31a53408f5c
mirachan010 <mirachan010@gmail.com>
2020-10-06 19:58:43 +0900

    生成場所変更

---
+++ DownloadfromGitHuba.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+for OPT in "$@"
+do
+    case $OPT in
+        -u)
+            USER=1
+            ORG=0
+            NAME=$2
+            ;;
+        -o)
+            USER=0
+            ORG=1
+            NAME=$2
+            ;;
+    esac
+    shift
+done
+touch ./${NAME}.list
+Max=`cat ./check.list |wc -l`
+COUNT=0
+VCOUNT=0
+COLOR=0
+if [ ! -d ./ReadMe/${NAME} ]; then
+    mkdir ./ReadMe/${NAME}
+fi
+while read line
+do
+    git clone git://github.com/${NAME}/$line --depth=1 > /dev/null 2>&1
+    if ls ./$line/colors/*.vim >/dev/null 2>&1;then
+        COLOR=1
+    else
+        COLOR=0
+    fi
+    if [ -d ./$line/autoload -o -d ./$line/plugin -o $COLOR -eq 1 -o -d ./$line/rplugin/ -o -d ./$line/lua ];then
+        echo ${NAME}/$line >> list_new
+        VCOUNT=$(( VCOUNT + 1 ))
+        COLOR=0
+        if [ -e ./$line/README.md ];then
+            cp ./$line/README.md ./ReadMe/${NAME}/$line
+        elif [ -e ./$line/README.mkd ]; then
+            cp ./$line/README.mkd ./ReadMe/${NAME}/$line
+        fi
+        touch ./ReadMe/${NAME}/$line
+    fi
+    COUNT=$(( COUNT + 1 ))
+    echo $COUNT/$Max -- $VCOUNT
+    rm -rf ./$line &
+    sleep 10 &
+    wait
+done < ./check.list
+rm ./check.list
+echo checking
+if [ -f ./list_new ];then
+    comm <(sort ./list_new) <(sort ./list) -23 >> list
+    rm ./list_new
+    sort -u list -o list
+    sort -u ./CheckedFiles/${NAME}.list -o ./CheckedFiles/${NAME}.list
+    git add ./list
+    git commit -m "add ${NAME}"
+    git add ./CheckedFiles/${NAME}.list
+    git commit -m "new repos ${NAME}"
+    git add ./ReadMe/${NAME}/.
+    git commit -m "new readme ${NAME}"
+fi
+echo finish
--- MakeInstallListForDein.sh
+++ MakeInstallListForDein.sh
@@ -5,7 +5,7 @@ do
     package=$( fzf -m --preview "head -100 {}" | awk '{print $1}' )
     cd ../
     if [ -n "$package" ]; then
-        echo "$package" | sed -e "s/$/\'/" -e "s/^/[[plugins]]\nrepo='/" >> ./list.toml
+        echo "$package" | sed -e "s/$/\'/" -e "s/^/[[plugins]]\nrepo='/" >> ~/list.toml
     else
         break
     fi
