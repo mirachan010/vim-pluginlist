634e13e5c3a6b7623a00761e2b29e999d13ea64b
mirachan010 <mirachan010@gmail.com>
2020-11-09 16:42:17 +0900

    update

--- MakeInstallListForPluginmanage.sh
+++ MakeInstallListForPluginmanage.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 read -p "Before plugin name>>" START
 read -p "After  plugin name>>" END
-while :
-do
-    cd ./ReadMe
-    package=$( fzf -m --preview "head -100 {}" | awk '{print $1}' )
-    cd ../
-    if [ -n "$package" ]; then
-        echo "$package" | sed -e "s/$/${END}/" -e "s/^/${START}/" >> ./list.vim
-    else
-        break
-    fi
-done
+cd ./ReadMe
+package=$( fzf -m --preview "head -100 {}" | awk '{print $1}' )
+cd ../
+if [ -n "$package" ]; then
+    echo "$package" | sed -e "s/$/${END}/" -e "s/^/${START}/" >> ./list.vim
+fi
